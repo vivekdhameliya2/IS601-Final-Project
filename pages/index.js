64e1bac33f726d09d78fc3a6fe68734e4e95fe56
BrijeshkumarSuvagiya <144753444+BrijeshkumarSuvagiya@users.noreply.github.com>
2023-11-23 19:59:30 -0500

    Close #22

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import Navbar from "../components/navbar";
 import Herosection from "../components/hero";
 import TitlebarBelowImageList from "../components/offer";
+import ImgMediaCard from "../components/menu"
 
 export default function Home() {
   return (
@@ -14,6 +15,12 @@ export default function Home() {
      <Herosection/>
      <div className={styles.centerContainer}>
      <TitlebarBelowImageList/>
+     </div>
+     <div className={styles.menucard}>
+     <ImgMediaCard/>
+     <ImgMediaCard/>
+     <ImgMediaCard/>
+     <ImgMediaCard/>
      </div>
     <div className={styles.container}>
       <Head>
