90a47c3fa28397bb8876e6a0b4329cd78d5c6d99
BrijeshkumarSuvagiya <144753444+BrijeshkumarSuvagiya@users.noreply.github.com>
2023-12-08 17:16:56 -0500

    Close #104

--- about/index.jsx
+++ about/index.jsx
@@ -1,6 +1,20 @@
 import Head from 'next/head';
 import Layout from '../../components/Layout';
 import aboutStyles from '../../assets/styles/about.module.scss';
+import Head from "next/head";
+import Layout from "../../components/Layout";
+import aboutStyles from "../../assets/styles/about.module.scss";
+import CommonBanner from "../../components/CommonBanner";
+import Link from "next/link";
+
+const breadcrumbs = [
+  <Link className={aboutStyles.breadcrumb_link} href="/">
+    Home
+  </Link>,
+  <Link className={aboutStyles.breadcrumb_link} href="/about">
+    About Us
+  </Link>,
+];
 
 
 export default function About() {
