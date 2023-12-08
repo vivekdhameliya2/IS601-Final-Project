ddaa978c36f2a7a1cbf130faf8da83007a5a05a2
BrijeshkumarSuvagiya <144753444+BrijeshkumarSuvagiya@users.noreply.github.com>
2023-12-08 11:39:08 -0500

    Close #87

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    transpilePackages: [
+      "antd",
+      "rc-util",
+      "@babel/runtime",
+      "@ant-design/icons",
+      "@ant-design/icons-svg",
+      "rc-pagination",
+      "rc-picker",
+      "rc-tree",
+      "rc-table",
+    ],
+  };
+
+  module.exports = nextConfig;
