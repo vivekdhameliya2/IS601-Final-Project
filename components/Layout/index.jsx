54587ce53c9f5f0c25d7e28b29d259ee4f2131ef
BrijeshkumarSuvagiya <144753444+BrijeshkumarSuvagiya@users.noreply.github.com>
2023-12-08 13:13:42 -0500

    Revert "Close #89"

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
-import Navbar from "./Navbar";
+import { Header } from "antd/es/layout/layout";
 import layout from "./layout.module.scss";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "../../assets/images/logo.png";
 
 const Layout = ({ children }) => {
-  const [isSticky, setIsSticky] = useState(false);
-
-  const handleScroll = (e) => {
-    const offset = e.target.scrollTop;
-    
-    if (offset > 2) {
-      setIsSticky(true);
-    } else {
-      setIsSticky(false);
-    }
-  };
 
   return (
-    <div onScroll={handleScroll} className={layout.root_layout}>
-      <Navbar isSticky={isSticky} />
+    <div className={layout.root_layout}>
+      <Header className={`${layout.header} custom-container`}>
+        <div className={layout.logo} ><Image src={logo} /></div>
+        <nav className={layout.navigation} >
+          <Link className={layout.active} href="/home">Home</Link>
+          <Link href="/about">About</Link>
+          <Link href="/pizza">Pizza</Link>
+          <Link href="/contact-us">Contact Us</Link>
+        </nav>
+      </Header>
       <main className={layout.content}>{children}</main>
       <footer>footer</footer>
     </div>
