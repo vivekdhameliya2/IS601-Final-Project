0fd927b68cf8e6b55d86a215d40f509729d6db19
BrijeshkumarSuvagiya <144753444+BrijeshkumarSuvagiya@users.noreply.github.com>
2023-12-15 09:51:50 -0500

    Close #211

--- design/playwright-tests/home.spec.js
+++ design/playwright-tests/home.spec.js
@@ -3,152 +3,78 @@ const { test, expect } = require("@playwright/test");
 const websiteURL = "http://localhost:3000";
 
 // Expected constants
-const expectedTitle = "Brijeshkumar suvagiya";
-const expectedLogoText = "BRIJESH";
-const expectedMenuItemCount = 4;
-const expectedHeroTitle = "Passionate Front End developer";
-const expectedHeroSubText = "Hello World! I’m";
-const expectedAboutTitle = "About Me";
-const expectedTestimonialsTitle = "Testimonial";
-const expectedTestimonialCount = 4;
-const expectedNavs = ["home", "about me", "portfolio", "Contact Me"];
-const expectedTestimonialNames = [
-  "Teri Dactyl",
-  "Teri Dactyl",
-  "Teri Dactyl",
-  "Teri Dactyl",
-];
-const expectedFooterLinks = ["home", "about me", "portfolio", "Testimonial"];
-const expectedSubmitButtonText = "Submit";
+const expectedSubTitle = "Todays Best Deals";
+const expectedDiscount = "50%";
+const expectedBtnText = "SIGNUP";
+const expectedSpecialMenuTitle = "From Italy Pizza, With Love";
+const expectedSpecialMenuSubTitle = "SPECIAL MENU";
+const testimonialTitle = "What Customer Say Us";
+const testmonialSubTitle = "OUR TESTIMONIAL";
+const deliveryTitle = "Your Favorite Pizza, On The Way!";
+const deliverySubTitle = "FASTEST DELIVERY";
+const viewAllUrl = "http://localhost:3000/menu";
 
-test("Test Basic HTML Structure", async ({ page }) => {
-  // Ensure the presence of <head> section
-  const headElement = await page.$("head");
-  expect(headElement).not.toBeNull();
+test("Check hero section", async ({ page }) => {
+  const subTitle = await page.$('[data-testid="hero-section-subtitle"]');
+  const subTitleText = await subTitle.textContent();
+  expect(subTitleText.trim().toLowerCase()).toBe(
+    expectedSubTitle.toLowerCase()
+  );
 
-  // Check for the presence of <title> element
-  const titleElement = await page.$("head title");
-  expect(titleElement).not.toBeNull();
+  const discount = await page.$('[data-testid="hero-section-discount"]');
+  const discountText = await discount.textContent();
+  expect(discountText.trim().toLowerCase()).toBe(
+    expectedDiscount.toLowerCase()
+  );
+
+  const btn = await page.$('[data-testid="hero-section-button"]');
+  const btnText = await btn.textContent();
+  expect(btnText.trim().toLowerCase()).toBe(expectedBtnText.toLowerCase());
 });
 
-test("Check Page Title", async ({ page }) => {
-  const title = await page.title();
-  expect(title).toBe(expectedTitle);
-});
+test("Check special menu section", async ({ page }) => {
+  const subTitle = await page.$('[data-testid="special-menu-subtitle"]');
+  const subTitleText = await subTitle.textContent();
+  expect(subTitleText.trim().toLowerCase()).toBe(
+    expectedSpecialMenuSubTitle.toLowerCase()
+  );
 
-test("Check Logo in Header", async ({ page }) => {
-  const logoText = (await page.locator(".logo").textContent()).trim();
-  expect(logoText).toBe(expectedLogoText);
-});
-
-test("Check Navigation Menu in Header", async ({ page }) => {
-  // Check nav menu counts
-  const menuCount = await page.locator(".menu-section .desk-nav ul li").count();
-  expect(menuCount).toBe(expectedMenuItemCount);
-
-  // Check nav menu content
-  const menuItems = await page.$$('.menu-section .desk-nav ul li');
-  for (let i = 0; i < menuItems.length; i++) {
-    const menuItem = menuItems[i];
-    const text = await menuItem.textContent();
-    expect(text.trim().toLowerCase()).toBe(expectedNavs[i].toLowerCase());
-  }
-});
-
-test("Check Hero Section", async ({ page }) => {
-  expect(
-    await page.locator(".tagline-section .tagline-info h2").textContent()
-  ).toBe(expectedHeroTitle);
-  expect(
-    await page.locator(".tagline-section .tagline-info span").textContent()
-  ).toBe(expectedHeroSubText);
-});
-
-test("Check About Section", async ({ page }) => {
-  expect(await page.locator(".about .content .title").textContent()).toBe(
-    expectedAboutTitle
+  const title = await page.$('[data-testid="special-menu-title"]');
+  const titleText = await title.textContent();
+  expect(titleText.trim().toLowerCase()).toBe(
+    expectedSpecialMenuTitle.toLowerCase()
   );
 });
 
-test("Check Testimonials Section title", async ({ page }) => {
-  // Check testimonial section title
-  expect(await page.locator(".newslatter .title").textContent()).toBe(
-    expectedTestimonialsTitle
-  );
-});
-
-test("Check Footer", async ({ page }) => {
-  // Check footer counts
-  const footerLinkscount = await page
-    .locator(".social-section nav ul li")
-    .count();
-  expect(footerLinkscount).toBeGreaterThan(0);
-
-  // Check footer links
-  
-  const footerLinks = await page.$$('.social-section nav ul li');
-  
-  for (let i = 0; i < footerLinks.length; i++) {
-    const footerLinkItem = footerLinks[i];
-    const text = await footerLinkItem.textContent();
-    expect(text.trim().toLowerCase()).toBe(
-      expectedFooterLinks[i].toLowerCase()
-    );
-  }
-});
-
-test("Check Newsletter Form Elements", async ({ page }) => {
-  const newsletterForm = await page.locator(".email-wrapper form");
-  const inputField = await newsletterForm.locator('input[type="email"]');
-  const submitButton = await newsletterForm.locator('button[type="submit"]');
-  expect(await inputField.isVisible()).toBe(true);
-  expect(await submitButton.isVisible()).toBe(true);
-  expect(await submitButton.getAttribute("value")).toBe(
-    expectedSubmitButtonText
-  );
-});
-
-test("Check All Navigation Links", async ({ page }) => {
-  const navLinks = await page.locator(".menu-section .desk-nav ul li");
-  const count = await navLinks.count();
-
-  for (let i = 0; i < count; i++) {
-    const link = navLinks.nth(i);
-    expect(await link.isVisible()).toBe(true);
-  }
-});
-
-test("Check All Testimonials count", async ({ page }) => {
-  const testimonials = await page.locator(
-    ".newslatter-wrapper .newslatter-card-layout"
+test("Check testimonial section", async ({ page }) => {
+  const subTitle = await page.$('[data-testid="testimonial-section-subtitle"]');
+  const subTitleText = await subTitle.textContent();
+  expect(subTitleText.trim().toLowerCase()).toBe(
+    testmonialSubTitle.toLowerCase()
   );
 
-  const count = await testimonials.count();
-
-  // Check testimonials count
-  expect(count).toBe(expectedTestimonialCount);
+  const title = await page.$('[data-testid="testimonial-section-title"]');
+  const titleText = await title.textContent();
+  expect(titleText.trim().toLowerCase()).toBe(testimonialTitle.toLowerCase());
 });
 
-test("Check All Testimonials content", async ({ page }) => {
-  for (let i = 1; i < 4; i++) {
-    const h3Selector = `.newslatter-wrapper .newslatter-card-layout:nth-child(${i}) .newslatter-card .newslatter-content h3`;
-    const h3 = await page.$(h3Selector);
-    const text = await h3.textContent();
+test("Check delivery section", async ({ page }) => {
+  const subTitle = await page.$('[data-testid="delivery-section-subtitle"]');
+  const subTitleText = await subTitle.textContent();
+  expect(subTitleText.trim().toLowerCase()).toBe(
+    deliverySubTitle.toLowerCase()
+  );
 
-    expect(text.trim().toLowerCase()).toBe(
-      expectedTestimonialNames[i].toLowerCase()
-    );
-  }
+  const title = await page.$('[data-testid="delivery-section-title"]');
+  const titleText = await title.textContent();
+  expect(titleText.trim().toLowerCase()).toBe(deliveryTitle.toLowerCase());
 });
 
-test("Check All Footer Links", async ({ page }) => {
-  const footerLinks = await page.locator(".social-section nav ul");
-  const count = await footerLinks.count();
-
-  for (let i = 0; i < count; i++) {
-    const link = footerLinks.nth(i);
-    expect(await link.isVisible()).toBe(true);
-  }
+test("Check view all redirect", async ({ page }) => {
+  await page.getByTestId("viewall-link").click();
+  await page.waitForURL(viewAllUrl);
+  const finalUrl = page.url();
+  expect(finalUrl).toBe(viewAllUrl);
 });
 
 test.beforeEach(async ({ page }) => {
