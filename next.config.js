c15b5b2d110fe1b79af567b1a9d7c13ae0d4e78e
Vivek <vad@njit.edu>
2023-12-16 14:38:19 -0500

    mailchimp url updated close #233

--- next.config.js
+++ next.config.js
@@ -14,8 +14,7 @@ const nextConfig = {
     DOMAIN: "italian-pizza-sigma.vercel.app",
     ANALITICS_ID: "G-KTDJQQD8CB",
     MAIL_CHIMP_URL:
-      "https://app.us13.list-manage.com/subscribe/post?u=4fd4ca3ebcd80644de9e8bcb1&amp;id=813b96f625&amp;f_id=00fdf1e7f0",
-  },
+      "https://gmail.us21.list-manage.com/subscribe/post?u=5526548e0995d18710f31f6d0&amp;id=e43cfb6601&amp;f_id=003aede6f0"},
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
