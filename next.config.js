ac67025a56cc4c0d6caa3d0f955f32458db83e10
Vivek <vad@njit.edu>
2023-12-16 14:42:55 -0500

    done close #236

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     NEXT_APP_URL: "https://is-601-final-project-drab.vercel.app/",
     CARD_IMAGE: "/images/hero-image.webp",
     DOMAIN: "is-601-final-project-drab.vercel.app",
-    ANALITICS_ID: "G-KTDJQQD8CB",
+    ANALITICS_ID: "G-0WCQ0QENB0",
     MAIL_CHIMP_URL:
       "https://gmail.us21.list-manage.com/subscribe/post?u=5526548e0995d18710f31f6d0&amp;id=e43cfb6601&amp;f_id=003aede6f0"},
   i18n: {
